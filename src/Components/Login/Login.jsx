73ad7dfd242cc5763aa0793a493bb6e10837a6a7
ajzal-byte <ajzalaju10@gmail.com>
2024-03-06 17:53:02 +0530

    Loading in login implementd

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -3,11 +3,15 @@ import "./Login.css";
 import { Logo } from "../../assets";
 import { FirebaseContext } from "../../store/FirebaseContext";
 import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+import Spinner from "../Spinner/Spinner";
 
 const Login = () => {
+  const [loading, setLoading] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { firebase } = useContext(FirebaseContext);
+  const navigate = useNavigate();
 
   const handleLogin = (e) => {
     e.preventDefault();
@@ -21,45 +25,57 @@ const Login = () => {
       return toast.error("Password should be at least 6 characters long.");
     }
 
+    setLoading(true);
+
     firebase
       .auth()
       .signInWithEmailAndPassword(email, password)
-      .then(() => toast.success("Logged In Successful"))
-      .catch((error) => toast.error(error.message));
+      .then(() => {
+        setLoading(false);
+        navigate("/");
+      })
+      .catch((error) => {
+        setLoading(false);
+        toast.error(error.message);
+      });
   };
 
   return (
     <div>
-      <div className="loginParentDiv">
-        <img width="200px" height="200px" src={Logo}></img>
-        <form onSubmit={handleLogin}>
-          <label htmlFor="fname">Email</label>
-          <br />
-          <input
-            className="input"
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            id="fname"
-            name="email"
-          />
-          <br />
-          <label htmlFor="lname">Password</label>
-          <br />
-          <input
-            className="input"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            id="lname"
-            name="password"
-          />
-          <br />
-          <br />
-          <button>Login</button>
-        </form>
-        <a>Signup</a>
-      </div>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className="loginParentDiv">
+          <img width="200px" height="200px" src={Logo}></img>
+          <form onSubmit={handleLogin}>
+            <label htmlFor="fname">Email</label>
+            <br />
+            <input
+              className="input"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              id="fname"
+              name="email"
+            />
+            <br />
+            <label htmlFor="lname">Password</label>
+            <br />
+            <input
+              className="input"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              id="lname"
+              name="password"
+            />
+            <br />
+            <br />
+            <button>Login</button>
+          </form>
+          <a>Signup</a>
+        </div>
+      )}
     </div>
   );
 };
