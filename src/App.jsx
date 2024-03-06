fd59d70a75d5da41bd74eb1e24148a13971a56ec
ajzal-byte <ajzalaju10@gmail.com>
2024-03-06 21:06:30 +0530

    Route added to create page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
-import { LoginPage, SignupPage } from "./Pages";
+import { CreatePage, LoginPage, SignupPage } from "./Pages";
 import { Toaster } from "react-hot-toast";
 import { AuthContext, FirebaseContext } from "./store/Context";
 
@@ -20,6 +20,7 @@ const App = () => {
           <Route exact path="/" element={<Home />}></Route>
           <Route path="/signup" element={<SignupPage />}></Route>
           <Route path="/login" element={<LoginPage />}></Route>
+          <Route path="/create" element={<CreatePage />}></Route>
         </Routes>
       </Router>
     </div>
